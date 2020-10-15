8bcfae087cefed1b9c979163000ad428f14508eb
Sierra117 <davids11703@gmail.com>
2020-10-15 08:11:36 +0200

    drillNo3 updated

--- source/Ch04/chapter/drillNo3.cpp
+++ source/Ch04/chapter/drillNo3.cpp
@@ -1,15 +1,43 @@
 #include "std_lib_facilities.h"
 //int a,b;
-double a,b,lk=9999999,ln=-99999999;
-char unit[10];
+double a,b,lk=9999999,ln=-99999999,sum=0;
+char unit=0;
 
 int main()
 {
       constexpr double cm_per_inch=2.54;
       constexpr double cm_per_meter=100;
       constexpr double inch_per_feet=12;
+      vector<double> minden;
     /*while(cin>>a>>b)*/ while(cin>>a>>unit)
     {
+        if(unit=='m')
+        {
+        	sum+=a;
+        	if(a<=lk)
+        	{
+        		lk=a;
+        	}
+        	if(a>=ln)
+        	{
+        		ln=a;
+        	}
+        	minden.push_back(a);
+        }
+        else if(unit='c')
+        {
+        	a=a*cm_per_meter;
+        		sum+=a;
+        	if(a<=lk)
+        	{
+        		lk=a;
+        	}
+        	if(a>=ln)
+        	{
+        		ln=a;
+        	}
+        	minden.push_back(a);
+        }
     	//cout<<a<<" "<<b; //elso feladat
     	/*if(a<b)
     	cout<<"The smaller value is: "<<a<<"\nThe larger value is: "<<b<<"\n";
@@ -31,5 +59,10 @@ int main()
     	}*/ //hatodik feladat
     	
     } 
+    cout<<"ln="<<ln<<"\n"<<"lk="<<lk<<"\n"<<"sum="<<sum<<"\n";
+    sort(minden);
+            for(double a : minden)
+            cout << a <<endl;
+            
     return 0;
 }
